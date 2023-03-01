335fb32e6a368d3b1860c5d4d9387b5ad6a875d9
JuandiGG <juandigascongonzalez@gmail.com>
2023-03-01 20:56:22 +0100

    Para subir

---
+++ src/app/hero.ts
@@ -0,0 +1,6 @@
+import { ImageLoader } from "@angular/common";
+
+export interface Hero {
+    id: number;
+    name: string;
+  }
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { IMAGE_LOADER } from '@angular/common';
+import { HtmlTagDefinition } from '@angular/compiler';
+import { config } from 'rxjs';
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Dr. Nice'},
+  { id: 2, name: 'Bombasto' },
+  { id: 3, name: 'Celeritas' },
+  { id: 4, name: 'Magneta' },
+  { id: 5, name: 'RubberMan' },
+  { id: 6, name: 'Dynama' },
+  { id: 7, name: 'Dr. IQ' },
+  { id: 8, name: 'Magma' },
+  { id: 9, name: 'Tornado' }
+];
